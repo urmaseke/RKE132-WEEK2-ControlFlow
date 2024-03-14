2ec37a5f2964fc24954caeba449976c8dbf34a34
Urmas Õunapu. <urmas.ounapuu@tktk.ee>
2024-03-14 04:08:28 +0200

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please, enter your age:");
+string Userage = Console.ReadLine();
+int Useragenum = 0;
+    bool isAgenumber =Int32.TryParse(Userage, out Useragenum);
+Console.WriteLine($"Parse result {isAgenumber}. User is {Useragenum} years old.");
+if (isAgenumber)
+{
+    if (Useragenum >= 13)
+    {
+        Console.WriteLine("Welcome to Instagram!");
+    }
+    else
+    {
+        Console.WriteLine("You are too young to use Instagram!");
+    }
+}
+else
+{
+    Console.WriteLine("Could not read your age.");
+}
+        
+      
+
---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please, select your gender (m/f)");
+char userGender = Char.Parse(Console.ReadLine());
+Console.WriteLine("Please, enter your last name:");
+string Userlastname=Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {Userlastname}!");
+}
+else if (userGender == 'f') 
+{
+    Console.WriteLine($"Welcome, Ms. {Userlastname}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {Userlastname}!");
+}
+
+       
+
+
+
